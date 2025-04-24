529f9c578408958acec7ebe28cd1d3351dc396c8
IraMae04 <iramaeramos04@gmail.com>
2025-04-24 15:00:35 +0800

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 const quizData = [
-    { question: 'Who painted the Mona Lisa', options: ['San Miguel', 'Da Vinci', 'Leonardo San Miguel', 'Juan Luna'], correctAnswer: 'Da Vinci' },
-    { question: 'What is 3 * 3?', options: ['9', '16', '12', '6'], correctAnswer: '9' },
-    { question: 'Who wrote "Romeo and Juliet"?', options: ['William Shakespeare', 'San Miguel', 'Andrei Jose', 'Jay Mark'], correctAnswer: 'William Shakespeare' }
+    { question: 'What is the largest planet in our solar system?', options: [' Earth', 'Mars', 'Jupiter', 'Venus'], correctAnswer: 'Jupiter' },
+    { question: 'Which color is commonly associated with danger?', options: ['Blue', 'Green', 'Red', 'Yellow'], correctAnswer: 'Red' },
+    { question: ' What is the smallest continent?', options: ['Asia', 'Africa', 'Australia', 'Europe'], correctAnswer: 'Australia' }
 ];
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
 
     const handleAnswerClick = (selectedAnswer) => {
         if (selectedAnswer === quizData[currentQuestion].correctAnswer) {
-            setScore(score + 1);
+            setScore(score + 1)
         }
         if (currentQuestion < quizData.length - 1) {
             setCurrentQuestion(currentQuestion + 1);
